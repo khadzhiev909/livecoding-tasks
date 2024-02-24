daea21373ca366b9b49908c6420da32dfa4a7bb7
Ependi Khadzhiev <khadzhiev909@gmail.com>
2024-02-25 02:06:00 +0300

    Исправления

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -82,25 +82,27 @@ class Main {
         int operand1, operand2, n = 0;
         char operator;
 
-        if (arr.length > 3 || arr.length < 3) {
-            throw new IllegalArgumentException("Много чисел");
+        if (arr.length != 3) {
+            throw new IllegalArgumentException("Неверное количество операндов и операторов");
         }
 
 
-        if (containsRomanNumber(arr[0]) && !containsRomanNumber(arr[2]) || containsRomanNumber(arr[2]) || !containsRomanNumber(arr[0])) {
+        if (containsRomanNumber(arr[0]) && !containsRomanNumber(arr[2]) || containsRomanNumber(arr[2]) && !containsRomanNumber(arr[0])) {
             throw new IllegalArgumentException("Оба операнды должны быть одного типа");
         } else if (containsRomanNumber(arr[0]) && containsRomanNumber(arr[2])) {
             n = 1;
             operand1 = romanToInt(arr[0]);
-        } else {
-            operand1 = Integer.parseInt(arr[0]);
-        }
-        if (containsRomanNumber(arr[2])) {
             operand2 = romanToInt(arr[2]);
         } else {
+            operand1 = Integer.parseInt(arr[0]);
             operand2 = Integer.parseInt(arr[2]);
         }
 
+//        if (containsRomanNumber(arr[2])) {
+//            operand2 = romanToInt(arr[2]);
+//        } else {
+//            operand2 = Integer.parseInt(arr[2]);
+//        }
         operator = arr[1].charAt(0);
 
         int result;
